754c58f95005186114826c037e10947f7785cc1d
Gary Marc Killen <killengary37@gmail.com>
2025-10-13 19:33:58 -0700

    add srgb color space

--- src/components/models/Macbook-14.jsx
+++ src/components/models/Macbook-14.jsx
@@ -11,14 +11,16 @@ Title: macbook pro M3 16 inch 2024
 import React, {useEffect} from 'react'
 import {useGLTF, useTexture} from '@react-three/drei'
 import useMacbookStore from "../../store/index.js";
-import {Color} from 'three';
+import {Color, SRGBColorSpace} from 'three';
 import {noChangeParts} from "../../constants/index.js";
 
 export default function MacbookModel14(props) {
     const { color } = useMacbookStore();
   const { nodes, materials, scene } = useGLTF('/models/macbook-14-transformed.glb');
 
-  const texture = useTexture('/screen.png')
+  const texture = useTexture('/screen.png');
+    texture.colorSpace = SRGBColorSpace;
+    texture.needsUpdate = true;
 
     useEffect(() => {
         scene.traverse((child) => {
